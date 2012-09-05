87241ef47f6d10be76d816db7760945fb7910fb3
Edgar Rodriguez <edgar.rd@gmail.com>
2012-09-05 00:36:31 -0400

    Adding build status and change to pass build

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 doko
 ====
 
+[![Build Status](https://secure.travis-ci.org/edgarRd/doko.png)](http://travis-ci.org/edgarRd/doko)
+
 Semantic Web Data Explorer for your console.
--- spec/lib/doko_spec.rb
+++ spec/lib/doko_spec.rb
@@ -4,6 +4,6 @@ describe Doko do
   # just a placeholder
   it "should do something" do
     something = nil
-    something.should_not be_nil
+    something.should be_nil
   end
 end
